d368b02e13f1172e2b8b07a271130d89691e3e73
Dulor Kisku <founder@tumdah.com>
2026-06-05 12:40:26 +0530

    style: format lib/core/utils/csv_helper_html.dart

--- lib/core/utils/csv_helper_html.dart
+++ lib/core/utils/csv_helper_html.dart
@@ -11,11 +11,11 @@ Future<void> saveAndShareCsv({
   final bytes = utf8.encode(csvContent);
   final blob = html.Blob([bytes], 'text/csv');
   final url = html.Url.createObjectUrlFromBlob(blob);
-  
+
   final anchor = html.AnchorElement(href: url)
     ..setAttribute('download', filename)
     ..style.display = 'none';
-    
+
   html.document.body?.append(anchor);
   anchor.click();
   anchor.remove();

3f943fb63053fe2e29d9d9c7a4014eeb97b36f07
Dulor Kisku <founder@tumdah.com>
2026-08-09 16:47:59 +0530

    fix(staging-health): use clean HttpOverrides without transitive platform interface imports

--- test/integration/appwrite_staging_smoke_test.dart
+++ test/integration/appwrite_staging_smoke_test.dart
@@ -1,15 +1,6 @@
 import 'dart:io';
 import 'package:appwrite/appwrite.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
-
-class _FakePathProvider extends Fake
-    with MockPlatformInterfaceMixin
-    implements PathProviderPlatform {
-  @override
-  Future<String?> getApplicationDocumentsPath() async => '.';
-}
 
 class _RealHttpOverrides extends HttpOverrides {
   @override
@@ -21,13 +12,10 @@ class _RealHttpOverrides extends HttpOverrides {
 }
 
 void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
-
   const endpoint = String.fromEnvironment('STAGING_APPWRITE_ENDPOINT');
   const projectId = String.fromEnvironment('STAGING_APPWRITE_PROJECT_ID');
 
   test('staging Appwrite endpoint responds to ping', () async {
-    PathProviderPlatform.instance = _FakePathProvider();
     HttpOverrides.global = _RealHttpOverrides();
 
     if (endpoint.isEmpty || projectId.isEmpty) {

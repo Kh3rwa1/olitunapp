9df0d96ff9c738d6c07aa2cc28eb3093a7383863
Dulor Kisku <founder@tumdah.com>
2026-08-20 11:12:07 +0530

    fix(test): remove unused integration_test imports

--- integration_test/auth_flow_test.dart
+++ integration_test/auth_flow_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:itun/features/auth/presentation/welcome_screen.dart';

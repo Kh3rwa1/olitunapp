b820185bb8fb60e6cc562c9b534c3cb7f082d5f4
Dulor Kisku <founder@tumdah.com>
2026-09-17 17:22:09 +0530

    style: fix dart formatting for CI

--- lib/shared/widgets/sharing/share_card_payload.dart
+++ lib/shared/widgets/sharing/share_card_payload.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 
-enum ShareCardKind {
-  quizResult,
-  streakMilestone,
-  badgeAchievement,
-}
+enum ShareCardKind { quizResult, streakMilestone, badgeAchievement }
 
 class ShareCardPayload {
   final ShareCardKind kind;
